81bf8b8ffb1568b9e797da412cfa9c7f28ebc41e
Gothelph <gothelph.dourden@gmail.com>
2026-01-16 17:00:56 +0300

    Bubble sort implementation

--- script.py
+++ script.py
@@ -1,10 +1,20 @@
 def sort(array):
-    pass
+    arr = array.copy()      # чтобы не менять исходный массив
+    n = len(arr)
+
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+    return arr
 
 
 def main():
     data = [5, 2, 9, 1, 3]
-    print(sort(data))
+    sorted_data = sort(data)
+    print(sorted_data)
 
 
 main()
+
